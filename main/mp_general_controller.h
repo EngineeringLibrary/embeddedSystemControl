0178f7887f476e691ec883ed36d6871a6d2f9660
André Dantas <lordcobisco@hotmail.com>
2017-12-08 13:35:54 -0300

    Criando alguns comentários para PI e general_controller.

--- main/mp_general_controller.h
+++ main/mp_general_controller.h
@@ -10,18 +10,18 @@ namespace ControlHandler{
     public:
     	MP_General_Controller(){}
 
-    	std::string setRestrictions(std::string restrictions);
-        std::string setControllerParameters(std::string controllers);
-    	virtual Type OutputControl(Type Reference, Type SignalInput) = 0;
+    	std::string setRestrictions(std::string restrictions); // Recebe uma string com R no final de cada conjunto de restrições. Converte cada trecho do início até encontrar o R em uma matriz e deleta o trecho da string.
+        std::string setControllerParameters(std::string controllers);// Recebe uma string com C no final de cada conjunto parâmetros do controlador afim por partes. Converte cada trecho do início até encontrar o C em uma matriz e deleta o trecho da string.
+    	virtual Type OutputControl(Type Reference, Type SignalInput) = 0; // Obriga as classes filhas a implementar essa função.
 
-    protected:
-    	LinAlg::Matrix<Type> *Restrictions;
-    	LinAlg::Matrix<Type> *controllerParameters;
-    	uint16_t quantityOfRegions;
-    	int inWitchRegion;
+    protected://significa que todas as classes filhas podem acessar os atributos abaixo.
+    	LinAlg::Matrix<Type> *Restrictions; // Vetor de Matrizes contendo todas as restrições para cada conjunto.
+    	LinAlg::Matrix<Type> *controllerParameters; // Vetor de Matrizes contendo todos os parâmetros de controlador para cada conjunto.
+    	uint16_t quantityOfRegions; // Quantidade de regiões em que o conjunto foi subdividido.
+    	int inWitchRegion; // Sinalisa em qual região pertence o estado calculado.
 
-    	bool isInside(const LinAlg::Matrix<Type> &_point);
-        bool any(const LinAlg::Matrix<Type> &H);
+    	bool isInside(const LinAlg::Matrix<Type> &_point);// Verifica se um estado pertence ao conjunto invariante.
+        bool any(const LinAlg::Matrix<Type> &H);// função para auxiliar isInside.
 	};
 }
 
--- main/mp_pi.h
+++ main/mp_pi.h
@@ -9,14 +9,14 @@ namespace ControlHandler{
     class MP_PI : public MP_General_Controller<Type>
     {
     public:
-        MP_PI(){this->step = 0.1; error = 1.0; integralError = 1.0;}
+        MP_PI(){this->step = 0.1; error = 1.0; integralError = 1.0;} //seta os parâmetros iniciais do controlador PI. Período de amostragem (step), erro e integral do erro.
 
-        Type OutputControl(Type Reference, Type SignalInput);
+        Type OutputControl(Type Reference, Type SignalInput); // implementa uma função obrigatória para herdar MP_General_Controller. É a única função que deve ser chamada no loop.
 
     protected:
-    	Type step, error, integralError, PIout, kp, ki, kd, bias;
+    	Type step, error, integralError, PIout, kp, ki, kd, bias; //Atributos de um controlador PI
     	
-    	void intError(void);
+    	void intError(void); //Método para calcular a integral do erro. OBS: Para calcular o pertencimento dos estados no conjunto invariante é necessário ter o PI calculado dessa forma. Não deve mudar a maneira como o cálculo é realizado.
 	};
 }
 
